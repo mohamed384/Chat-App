11083fb214860a42a075f722e6944941c8221fb4
mohamed ezz <mohamed384@github.com>
2024-01-07 20:04:17 +0200

    signUp and SignIn gui update

--- rmi-modules/Client/src/main/java/org/example/Client.java
+++ rmi-modules/Client/src/main/java/org/example/Client.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.example.interfaces.UserAuthentication;
 
 import java.rmi.Naming;
 import java.util.List;
 
-public class Client {
+public class Client extends Application {
 
     public static void main(String[] args) {
+        launch();
         try {
             UserAuthentication remoteObject = (UserAuthentication) Naming.lookup("rmi://localhost:1099/rmiObject");
             boolean x=false;
@@ -17,5 +23,15 @@ public class Client {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("/views/Register.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
