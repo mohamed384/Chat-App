db4b250b66e78fd94fece3d2ea1ff05863a4f437
mohamed ezz <58311165+mohamed384@users.noreply.github.com>
2024-01-12 15:08:00 +0200

    Delete rmi-modules directory
    start a new branches

--- rmi-modules/Shared/src/main/java/org/example/interfaces/UserAuthentication.java
+++
@@ -1,8 +0,0 @@
-package org.example.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface UserAuthentication extends Remote {
-    public boolean signup(User user) throws RemoteException;
-}
--- rmi-modules/Shared/src/main/java/org/example/interfaces/UserStatus.java
+++
@@ -1,8 +0,0 @@
-package org.example.interfaces;
-
-public enum UserStatus {
-    OFFLINE,
-    AVAILABLE,
-    BUSY,
-    AWAY
-}
