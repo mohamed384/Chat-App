05670cfcf5d8f157e873e77354f5a78db8b7c7ae
EslaM-AhMed14 <eslamlahmed14@gmail.com>
2024-01-25 02:30:05 +0200

    add Contact Page

---
+++ rmi-modules/Client/src/main/java/org/example/controller/FXMLController/ContactController.java
@@ -0,0 +1,14 @@
+package org.example.controller.FXMLController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class ContactController {
+
+    @FXML
+    private ListView<?> listView;
+
+}
+
